332b23a8174d1f686bc3f766ed23ea427cfa7f18
izZERO <crusherh7x@hotmail.com>
2025-09-11 21:49:20 +0300

    1- refactored pushLetters function
    2- implemented deleteLetter

--- script.js
+++ script.js
@@ -11,25 +11,43 @@ const sqrEl = document.querySelectorAll(".sqr")
 
 // Functions
 const getIndex = (row, col) => row * cols + col
+
 const pushLetter = (letter) => {
   let index = getIndex(currentRow, currentCol)
-  sqrEl[index].textContent = letter
-  if (currentCol === 4 && currentRow != 5) {
-    currentRow++
-    currentCol = 0
-  } else if (currentCol === 4 && currentRow === 5) {
-    console.log("end reached")
-    // TODO: CALL LOST FUNCTION HERE
+
+  if (currentRow <= 5) {
+    sqrEl[index].textContent = letter
+    if (currentCol === 4) {
+      currentRow++
+      currentCol = 0
+    } else {
+      currentCol++
+    }
   } else {
-    currentCol++
+    console.log("end reached")
   }
+}
+
+const deleteLetter = () => {
+  let index = getIndex(currentRow, currentCol)
+
+  if (!(currentCol === 0 && currentRow === 0)) {
+    if (currentCol === 0) {
+      sqrEl[index - 1].textContent = ""
+      currentCol = 4
+      currentRow--
+    } else {
+      sqrEl[index - 1].textContent = ""
+      currentCol--
+    }
+  } else console.log("cannot delete any further")
 }
 
 document.addEventListener("keydown", (event) => {
   if (event.key === "Enter") {
     console.log("submit guess")
   } else if (event.key === "Backspace") {
-    console.log("delete character")
+    deleteLetter()
   } else if (charRegex.test(event.key)) {
     pushLetter(event.key)
   }
