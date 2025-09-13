3f267a58b7ad365131745f1463658d82dcdc48e0
izZERO <crusherh7x@hotmail.com>
2025-09-13 20:45:51 +0300

    added letterOccurrences check for misplaced letters

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Global variables
-const wordle = wordsList[Math.floor(Math.random() * 2301)].toUpperCase()
+const wordle = "APPLE" //wordsList[Math.floor(Math.random() * 2301)].toUpperCase()
+const wordleArr = wordle.split("")
 const rows = 6
 const cols = 5
 const charRegex = /^[A-Za-z]$/
@@ -8,6 +9,7 @@ let currentRow = 0
 let currentCol = 0
 let guessArr = []
 let correctGuess = []
+let letterOccurrences = {}
 console.log(wordle)
 
 // Elements
@@ -23,15 +25,6 @@ const pushLetter = (letter) => {
     sqrEl[index].textContent = letter.toUpperCase()
     guessArr.push(letter.toUpperCase())
     currentCol++
-
-    //   if (currentCol === 4) {
-    //     currentRow++
-    //     currentCol = 0
-    //   } else {
-    //     currentCol++
-    //   }
-    // } else {
-    //   console.log("end reached")
   }
 }
 
@@ -55,9 +48,21 @@ const handleLoss = () => {
   alert("you lost the game")
 }
 
+const countLetterOccurrences = () => {
+  wordleArr.forEach((letter) => {
+    if (!letterOccurrences[letter]) {
+      letterOccurrences[letter] = 1
+    } else {
+      letterOccurrences[letter]++
+    }
+  })
+  console.log(letterOccurrences)
+}
+
 const submitGuess = () => {
   if (guessArr.length === 0) return
 
+  const localOccurrences = { ...letterOccurrences }
   let guess = guessArr.join("")
 
   // if (!wordsList.includes(guess.toLowerCase())) {
@@ -65,15 +70,25 @@ const submitGuess = () => {
   //   return
   // }
 
-  // check letters
+  // check correct letters
   guessArr.forEach((letter, index) => {
     let sqrIndex = getIndex(currentRow, index)
-    if (wordle.includes(letter)) {
-      if (letter === wordle[index]) {
-        sqrEl[sqrIndex].classList.add("correct")
-      } else {
-        sqrEl[sqrIndex].classList.add("wrong-place")
-      }
+
+    if (letter === wordle[index]) {
+      sqrEl[sqrIndex].classList.add("correct")
+      localOccurrences[letter]--
+    }
+  })
+  // check wrong placed letters
+  guessArr.forEach((letter, index) => {
+    let sqrIndex = getIndex(currentRow, index)
+
+    // skip already correct
+    if (sqrEl[sqrIndex].classList.contains("correct")) return
+
+    if (wordle.includes(letter) && localOccurrences[letter] > 0) {
+      sqrEl[sqrIndex].classList.add("wrong-place")
+      localOccurrences[letter]--
     } else {
       sqrEl[sqrIndex].classList.add("wrong")
     }
@@ -85,7 +100,7 @@ const submitGuess = () => {
 
   if (guess === wordle) {
     handleWin()
-  } else if (currentRow === 6) {
+  } else if (currentRow === rows) {
     handleLoss()
   }
 }
@@ -101,3 +116,5 @@ document.addEventListener("keydown", (event) => {
     }
   }
 })
+
+countLetterOccurrences()
