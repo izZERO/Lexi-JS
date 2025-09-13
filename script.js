6ace33b6f6a0c4c5e91af87cd796683a4d8229c6
izZERO <crusherh7x@hotmail.com>
2025-09-13 16:08:29 +0300

    1- added submitGuess function
    2- refactored old functions

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 // Global variables
-const wordle = wordsList[Math.floor(Math.random() * 2301)]
+const wordle = wordsList[Math.floor(Math.random() * 2301)].toUpperCase()
 const rows = 6
 const cols = 5
 const charRegex = /^[A-Za-z]$/
+let isGameOver = false
 let currentRow = 0
 let currentCol = 0
+let guessArr = []
+let correctGuess = []
+console.log(wordle)
 
 // Elements
 const sqrEl = document.querySelectorAll(".sqr")
@@ -15,40 +19,87 @@ const getIndex = (row, col) => row * cols + col
 const pushLetter = (letter) => {
   let index = getIndex(currentRow, currentCol)
 
-  if (currentRow <= 5) {
-    sqrEl[index].textContent = letter
-    if (currentCol === 4) {
-      currentRow++
-      currentCol = 0
-    } else {
-      currentCol++
-    }
-  } else {
-    console.log("end reached")
+  if (currentRow < 6 && currentCol < 5) {
+    sqrEl[index].textContent = letter.toUpperCase()
+    guessArr.push(letter.toUpperCase())
+    currentCol++
+
+    //   if (currentCol === 4) {
+    //     currentRow++
+    //     currentCol = 0
+    //   } else {
+    //     currentCol++
+    //   }
+    // } else {
+    //   console.log("end reached")
   }
 }
 
 const deleteLetter = () => {
   let index = getIndex(currentRow, currentCol)
+  let rowOfIndex = Math.floor((index - 1) / cols)
+  if (rowOfIndex === currentRow) {
+    sqrEl[index - 1].textContent = ""
+    guessArr.pop()
+    currentCol--
+  }
+}
 
-  if (!(currentCol === 0 && currentRow === 0)) {
-    if (currentCol === 0) {
-      sqrEl[index - 1].textContent = ""
-      currentCol = 4
-      currentRow--
+const handleWin = () => {
+  isGameOver = true
+  alert("you have Won the game!!!")
+}
+
+const handleLoss = () => {
+  isGameOver = true
+  alert("you lost the game")
+}
+
+const submitGuess = () => {
+  if (guessArr.length === 0) return
+
+  let guess = guessArr.join("").toLowerCase()
+
+  if (!wordsList.includes(guess)) {
+    alert("Word doesn't exist!")
+    return
+  }
+
+  if (guess == wordle) {
+    handleWin()
+    return
+  }
+  // check letters
+  guessArr.forEach((letter, index) => {
+    if (wordle.includes(letter)) {
+      if (letter === wordle[index]) {
+        console.log(`${letter} is correct`)
+      } else {
+        console.log(`${letter} is correct but wrong place`)
+      }
     } else {
-      sqrEl[index - 1].textContent = ""
-      currentCol--
+      console.log(`${letter} is wrong`)
     }
-  } else console.log("cannot delete any further")
+  })
+
+  currentRow++
+  currentCol = 0
+  guessArr = []
+
+  if (currentRow === 6) {
+    handleLoss()
+    return
+  }
 }
 
 document.addEventListener("keydown", (event) => {
-  if (event.key === "Enter") {
-    console.log("submit guess")
-  } else if (event.key === "Backspace") {
-    deleteLetter()
-  } else if (charRegex.test(event.key)) {
-    pushLetter(event.key)
+  if (!isGameOver) {
+    if (event.key === "Enter") {
+      submitGuess()
+    } else if (event.key === "Backspace") {
+      deleteLetter()
+    } else if (charRegex.test(event.key)) {
+      pushLetter(event.key)
+    }
   }
 })
